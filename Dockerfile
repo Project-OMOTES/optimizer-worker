d02b7fff1d38aaea28e101a143d9331e122665e9
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2023-09-15 12:08:44 +0200

    Run rabbitmq and calculations in seperate threads.

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,12 @@ WORKDIR /app
 
 RUN mkdir /app/input_esdl_files/ /app/output_esdl_files/
 ENV PYTHONPATH='/app/'
+ENV INPUT_FILES_DIR="/app/input_esdl_files/"
+ENV OUTPUT_FILES_DIR="/app/output_esdl_files/"
 
 COPY requirements.txt /app/optimizer_worker/requirements.txt
-RUN pip install -r /app/optimizer_worker/requirements.txt
+RUN pip install -r /app/optimizer_worker/requirements.txt && \
+    pip cache purge
 
 COPY src/optimizer_worker                 /app/optimizer_worker/
 COPY src/optimization_runner              /app/optimization_runner/
--- src/optimizer_worker/main.py
+++ src/optimizer_worker/main.py
@@ -1,9 +1,11 @@
 import signal
+import threading
 from dataclasses import dataclass
 import json
 import logging
 import os
 import subprocess
+from pathlib import Path
 from typing import Optional
 import uuid
 
@@ -40,10 +42,100 @@ class CalculationResult:
     output_esdl: Optional[str]
 
 
+class PerformCalculation(threading.Thread):
+    nwn_client: NwnClient
+    input_files_dir: Path
+    output_files_dir: Path
+    job_id: uuid.UUID
+    channel: pika.adapters.blocking_connection.BlockingChannel
+    method: pika.spec.Basic.Deliver
+
+    def __init__(
+        self,
+        nwn_client: NwnClient,
+        input_files_dir: Path,
+        output_files_dir: Path,
+        job_id: uuid.UUID,
+        channel: pika.adapters.blocking_connection.BlockingChannel,
+        method: pika.spec.Basic.Deliver,
+    ):
+        super().__init__()
+
+        self.nwn_client = nwn_client
+        self.input_files_dir = input_files_dir
+        self.output_files_dir = output_files_dir
+        self.job_id = job_id
+        self.channel = channel
+        self.method = method
+
+    def run(self) -> None:
+        LOGGER.info("Starting calculation for job %s", self.job_id)
+        input_esdl_string = self.nwn_client.get_job_input_esdl(self.job_id)
+        self.nwn_client.set_job_running(self.job_id)
+        calculation_result = self.run_optimizer_calculation(
+            self.input_files_dir, self.output_files_dir, self.job_id, input_esdl_string
+        )
+        self.store_calculation_result(calculation_result)
+        if self.channel.is_open:
+            self.channel.connection.add_callback_threadsafe(lambda: self.channel.basic_ack(self.method.delivery_tag))
+        else:
+            LOGGER.warning("Rabbitmq channel was closed so could not ack that job %s was performed.", self.job_id)
+
+    def store_calculation_result(self, calculation_result: CalculationResult) -> None:
+        new_status = JobStatus.FINISHED if calculation_result.exit_code == 0 else JobStatus.ERROR
+
+        self.nwn_client.store_job_result(
+            job_id=calculation_result.job_id,
+            new_logs=calculation_result.logs,
+            new_status=new_status,
+            output_esdl=calculation_result.output_esdl,
+        )
+
+    @staticmethod
+    def run_optimizer_calculation(
+        input_files_dir: Path, output_files_dir: Path, job_id: uuid.uuid4, esdl_string: str
+    ) -> CalculationResult:
+        input_file = input_files_dir / f"{job_id}.esdl"
+        output_file = output_files_dir / f"{job_id}.esdl"
+
+        with open(input_file, "w+") as open_esdl_input_file:
+            open_esdl_input_file.write(esdl_string)
+
+        with TempEnvVar("INPUT_ESDL_FILE_NAME", str(input_file)):
+            with TempEnvVar("OUTPUT_ESDL_FILE_NAME", str(output_file)):
+                process_result = subprocess.run(
+                    ["python3", "-m", "optimization_runner.run_optimizer"],
+                    stderr=subprocess.STDOUT,
+                    stdout=subprocess.PIPE,
+                    text=True,
+                )
+
+        if process_result.returncode == 0:
+            with open(output_file) as open_esdl_output_file:
+                output_esdl = open_esdl_output_file.read()
+        else:
+            output_esdl = None
+
+        LOGGER.info("Completed job %s with exit_code %s", job_id, process_result.returncode)
+
+        return CalculationResult(
+            job_id=job_id,
+            logs=process_result.stdout,
+            exit_code=process_result.returncode,
+            input_esdl=esdl_string,
+            output_esdl=output_esdl,
+        )
+
+
 class OptimizationWorker:
     nwn_client: NwnClient
+    input_files_dir: Path
+    output_files_dir: Path
+    running_calculation: Optional[PerformCalculation]
 
-    def __init__(self):
+    def __init__(self, input_files_dir: Path, output_files_dir: Path):
+        self.input_files_dir = input_files_dir
+        self.output_files_dir = output_files_dir
         postgres_config = PostgresConfig(
             host=os.environ.get("POSTGRES_HOST", "localhost"),
             port=os.environ.get("POSTGRES_PORT", "6432"),
@@ -61,8 +153,9 @@ class OptimizationWorker:
         )
 
         self.nwn_client = NwnClient(postgres_config=postgres_config, rabbitmq_config=rabbitmq_config)
+        self.running_calculation = None
 
-    def on_message(
+    def on_start_workflow_message(
         self,
         ch: pika.adapters.blocking_connection.BlockingChannel,
         method: pika.spec.Basic.Deliver,
@@ -71,22 +164,24 @@ class OptimizationWorker:
     ):
         message = json.loads(body.decode("utf-8"))
         job_id = uuid.UUID(message.get("job_id"))
+
         if job_id is None:
             LOGGER.error("Received a message which did not contain a job id. Message: %s", message)
+            ch.connection.add_callback_threadsafe(lambda: ch.basic_ack(method.delivery_tag))
         else:
             LOGGER.info("Received message to work on job %s", job_id)
-            input_esdl_string = self.nwn_client.get_job_input_esdl(job_id)
-            self.nwn_client.set_job_running(job_id)
-            calculation_result = self.run_optimizer_calculation(job_id, input_esdl_string)
-            self.store_calculation_result(calculation_result)
-        ch.basic_ack(method.delivery_tag)
+            self.running_calculation = PerformCalculation(
+                self.nwn_client, self.input_files_dir, self.output_files_dir, job_id, ch, method
+            )
+            self.running_calculation.start()
 
     def work(self):
         signal.signal(signal.SIGQUIT, self.signal_handler)
         signal.signal(signal.SIGINT, self.signal_handler)
         signal.signal(signal.SIGTERM, self.signal_handler)
         self.nwn_client.connect()
-        self.nwn_client.wait_for_work({Queue.StartWorkflowOptimizer: self.on_message})
+        self.nwn_client.set_callbacks({Queue.StartWorkflowOptimizer: self.on_start_workflow_message})
+        self.nwn_client.join()
 
     def signal_handler(self, signal, frame):
         LOGGER.info("Received signal %s. Stopping..", signal)
@@ -95,49 +190,9 @@ class OptimizationWorker:
     def stop(self):
         self.nwn_client.stop()
 
-    def store_calculation_result(self, calculation_result: CalculationResult) -> None:
-        new_status = JobStatus.FINISHED if calculation_result.exit_code == 0 else JobStatus.ERROR
-
-        self.nwn_client.store_job_result(
-            job_id=calculation_result.job_id,
-            new_logs=calculation_result.logs,
-            new_status=new_status,
-            output_esdl=calculation_result.output_esdl,
-        )
-
-    @staticmethod
-    def run_optimizer_calculation(job_id: uuid.uuid4, esdl_string: str) -> CalculationResult:
-        with open(f"/app/input_esdl_files/{job_id}.esdl", "w+") as open_esdl_input_file:
-            open_esdl_input_file.write(esdl_string)
-
-        with TempEnvVar("INPUT_ESDL_FILE_NAME", f"/app/input_esdl_files/{job_id}.esdl"):
-            with TempEnvVar("OUTPUT_ESDL_FILE_NAME", f"/app/output_esdl_files/{job_id}.esdl"):
-                process_result = subprocess.run(
-                    ["python3", "-m", "optimization_runner.run_optimizer"],
-                    stderr=subprocess.STDOUT,
-                    stdout=subprocess.PIPE,
-                    text=True,
-                )
-
-        if process_result.returncode == 0:
-            with open(f"/app/output_esdl_files/{job_id}.esdl") as open_esdl_output_file:
-                output_esdl = open_esdl_output_file.read()
-        else:
-            output_esdl = None
-
-        LOGGER.info("Completed job %s with exit_code %s", job_id, process_result.returncode)
-
-        return CalculationResult(
-            job_id=job_id,
-            logs=process_result.stdout,
-            exit_code=process_result.returncode,
-            input_esdl=esdl_string,
-            output_esdl=output_esdl,
-        )
-
 
 def main():
-    OptimizationWorker().work()
+    OptimizationWorker(Path(os.environ["INPUT_FILES_DIR"]), Path(os.environ["OUTPUT_FILES_DIR"])).work()
 
 
 if __name__ == "__main__":
