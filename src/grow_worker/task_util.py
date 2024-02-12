05b1ddbbf318a86d63c324ef3e45fc05073a387e
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-02-12 11:44:48 +0100

    28: Moved all boilerplate code to omotes-sdk-python.

--- src/grow_worker/task_util.py
+++
@@ -1,9 +0,0 @@
-import celery
-
-
-class TaskUtil:
-    def __init__(self, task: celery.Task):
-        self.task = task
-
-    def update_progress(self, fraction: float, message: str) -> None:
-        self.task.send_event("task-progress-update", progress={"fraction": fraction, "message": message})
--- src/grow_worker/worker.py
+++ src/grow_worker/worker.py
@@ -1,8 +1,4 @@
 import base64
-import io
-import pickle
-import sys
-import socket
 
 import jsonpickle
 import os
@@ -10,130 +6,26 @@ import logging
 from typing import Optional, Callable, Any
 from uuid import uuid4
 from pathlib import Path
-from celery import Celery, Task as CeleryTask
 from celery.signals import after_setup_logger, worker_shutting_down
-from celery.apps.worker import Worker as CeleryWorker
-from kombu import Queue as KombuQueue
-from dataclasses import dataclass
 
-from omotes_job_tools.messages import StatusUpdateMessage, TaskStatus, CalculationResult
+from omotes_sdk.internal.orchestrator_worker_events.messages import (
+    StatusUpdateMessage,
+    TaskStatus,
+    CalculationResult,
+)
+from omotes_sdk.internal.worker.worker import WorkerTask, initialize_worker, UpdateProgressHandler
 from rtctools_heat_network.workflows import run_end_scenario_sizing
-from omotes_job_tools.broker_interface import BrokerInterface
-from omotes_job_tools.config import RabbitMQConfig as EmptyRabbitMQConfig
 
-from grow_worker.task_util import TaskUtil
-from grow_worker.worker_types import TaskType, GROWProblem, get_problem_type
+from grow_worker.worker_types import GrowTaskType, GROWProblem, get_problem_type
 
 
-class RabbitMQConfig(EmptyRabbitMQConfig):
-    def __init__(self):
-        super().__init__(
-            host=os.environ.get("RABBITMQ_HOST", "localhost"),
-            port=int(os.environ.get("RABBITMQ_PORT", "5672")),
-            username=os.environ.get("RABBITMQ_USERNAME"),
-            password=os.environ.get("RABBITMQ_PASSWORD"),
-            virtual_host=os.environ.get("RABBITMQ_VIRTUALHOST", "omotes_celery"),
-        )
+logger = logging.getLogger("grow_worker")
+GROW_TASK_TYPE = GrowTaskType(os.environ.get("GROW_WORKER_TASK_TYPE"))
 
 
-class PostgreSQLConfig:
-    host: str = os.environ.get("POSTGRESQL_HOST", "localhost")
-    port: int = int(os.environ.get("POSTGRESQL_PORT", "5672"))
-    database: str = os.environ.get("POSTGRESQL_DATABASE", "omotes_celery")
-    username: Optional[str] = os.environ.get("POSTGRESQL_USERNAME")
-    password: Optional[str] = os.environ.get("POSTGRESQL_PASSWORD")
-
-
-class WorkerConfig:
-    rabbitmq: RabbitMQConfig = RabbitMQConfig()
-    postgresql: PostgreSQLConfig = PostgreSQLConfig()
-    task_event_queue_name: str = os.environ.get("TASK_EVENT_QUEUE_NAME", "omotes_task_events")
-    task_type: TaskType = TaskType(os.environ.get("WORKER_TASK_TYPE"))
-    log_level: str = os.environ.get("LOG_LEVEL", "INFO")
-
-
-logger = logging.getLogger()
-
-WORKER: "Worker" = None
-
-
-class WrappedTask(CeleryTask):
-    def on_timeout(self, soft, timeout):
-        super().on_timeout(soft, timeout)
-        if not soft:
-            logger.warning("A hard timeout was enforced for task %s", self.task.name)
-
-    def on_failure(self, exc, task_id, args, kwargs, einfo):
-        super().on_failure(exc, task_id, args, kwargs, einfo)
-        logger.error("Failure detected for celery task %s", task_id)
-        # TODO Entrypoint to notify orchestrator & sdk of failure of task. At least in case where
-        #  Celery itself or the task triggers an error. This is necessary as task is dropped but an
-        #  error is published to logs. SDK wouldn't be notified otherwise.
-
-
-class Worker:
-    config = WorkerConfig()
-    captured_logging_string = io.StringIO()
-
-    celery_app: Celery
-    celery_worker: CeleryWorker
-
-    def _output_root_logger_to_stdout(self):
-        root_logger = logging.getLogger()
-        sys_stream_handler = logging.StreamHandler(sys.stdout)
-        root_logger.addHandler(sys_stream_handler)
-        root_logger.setLevel(self.config.log_level)
-
-    def start(self):
-        self._output_root_logger_to_stdout()
-
-        config = self.config
-        self.celery_app = Celery(
-            "omotes",
-            broker=f"amqp://{config.rabbitmq.username}:{config.rabbitmq.password}@{config.rabbitmq.host}:{config.rabbitmq.port}/{config.rabbitmq.virtual_host}",
-            backend=f"db+postgresql://{config.postgresql.username}:{config.postgresql.password}@{config.postgresql.host}:{config.postgresql.port}/{config.postgresql.database}",
-        )
-
-        # Config of celery app
-        self.celery_app.conf.task_queues = (
-            KombuQueue(config.task_type.value, routing_key=config.task_type.value),
-        )  # Tell the worker to listen to a specific queue for 1 workflow type.
-        self.celery_app.conf.task_acks_late = True
-        self.celery_app.conf.task_reject_on_worker_lost = True
-        self.celery_app.conf.task_acks_on_failure_or_timeout = False
-        self.celery_app.conf.worker_prefetch_multiplier = 1
-        self.celery_app.conf.broker_connection_retry_on_startup = True
-        # app.conf.worker_send_task_events = True  # Tell the worker to send task events.
-        WrappedTask.worker = self
-        self.celery_app.task(_grow_worker_task, base=WrappedTask, name=config.task_type.value, bind=True)
-
-        logger.info("Starting GROW worker to work on task %s", config.task_type.value)
-        logger.info(
-            "Connected to broker rabbitmq (%s:%s/%s) as %s",
-            config.rabbitmq.host,
-            config.rabbitmq.port,
-            config.rabbitmq.virtual_host,
-            config.rabbitmq.username,
-        )
-
-        self.celery_worker = self.celery_app.Worker(
-            hostname=f"worker-{config.task_type.value}@{socket.gethostname()}",
-            log_level=logging.getLevelName(config.log_level),
-            autoscale=(1, 1),
-        )
-
-        self.celery_worker.start()
-
-        # logger.info(
-        #     "Connected to backend postgresql (%s:%s/%s) as %s",
-        #     config.postgresql.host,
-        #     config.postgresql.port,
-        #     config.postgresql.database,
-        #     config.postgresql.username,
-        # )
-
-
-def _grow_worker_task(task: WrappedTask, job_id: uuid4, esdl_string: bytes) -> Any:
+def grow_worker_task(
+    task: WorkerTask, job_id: uuid4, esdl_string: bytes, update_progress_handler: UpdateProgressHandler
+) -> Any:
     """
 
     Note: Be careful! This spawns within a subprocess and gains a copy of memory from parent
@@ -146,39 +38,11 @@ def _grow_worker_task(task: WrappedTask, job_id: uuid4, esdl_string: bytes) -> A
     :param esdl_string:
     :return: Pickled Calculation result
     """
-    with BrokerInterface(config=WORKER.config.rabbitmq) as broker_if:
-        # global logging_string
-        # logging_string = io.StringIO()
-        logger.info("GROW worker started new task %s", job_id)
-        broker_if.send_message_to(
-            WORKER.config.task_event_queue_name,
-            pickle.dumps(
-                StatusUpdateMessage(
-                    omotes_job_id=job_id,
-                    celery_task_id=task.request.id,
-                    status=TaskStatus.STARTED,
-                    task_type=WORKER.config.task_type.value,
-                ).to_dict()
-            ),
-        )
-        result = rtc_calculate(job_id, esdl_string, WORKER.config.task_type, TaskUtil(task).update_progress)
-        broker_if.send_message_to(
-            WORKER.config.task_event_queue_name,
-            pickle.dumps(
-                StatusUpdateMessage(
-                    omotes_job_id=job_id,
-                    celery_task_id=task.request.id,
-                    status=TaskStatus.SUCCEEDED,
-                    task_type=WORKER.config.task_type.value,
-                ).to_dict()
-            ),
-        )
+    result = rtc_calculate(job_id, esdl_string, update_progress_handler)
     return jsonpickle.encode(result)
 
 
-def rtc_calculate(
-    job_id: uuid4, encoded_esdl: bytes, task_type: TaskType, update_progress: Callable[[float, str], None]
-) -> CalculationResult:
+def rtc_calculate(job_id: uuid4, encoded_esdl: bytes, update_progress: UpdateProgressHandler) -> CalculationResult:
     # try:
     esdl_string = encoded_esdl.decode()
     base_folder = Path(__file__).resolve().parent.parent
@@ -189,7 +53,7 @@ def rtc_calculate(
     logger.info(f"Will write result profiles to influx: {write_result_db_profiles}. At {influxdb_host}:{influxdb_port}")
 
     solution: GROWProblem = run_end_scenario_sizing(
-        get_problem_type(task_type),
+        get_problem_type(GROW_TASK_TYPE),
         base_folder=base_folder,
         esdl_string=base64.encodebytes(esdl_string.encode("utf-8")),
         write_result_db_profiles=write_result_db_profiles,
@@ -242,5 +106,4 @@ def setup_loggers(logger, *args, **kwargs):
 
 
 if __name__ == "__main__":
-    WORKER = Worker()
-    WORKER.start()
+    initialize_worker(GROW_TASK_TYPE.value, grow_worker_task)
