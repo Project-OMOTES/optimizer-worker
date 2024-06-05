877a40da9dc33f5e02e2604500dcb2d8b625bc4b
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-06-05 18:18:07 +0200

    55: Move mesido to run in a separate subprocess but acknowlodging this may lead to zombie processes.

--- src/grow_worker/worker.py
+++ src/grow_worker/worker.py
@@ -1,7 +1,8 @@
 import base64
-
-import os
 import logging
+import multiprocessing
+from multiprocessing.process import current_process
+import os
 from pathlib import Path
 from typing import cast
 
@@ -18,7 +19,49 @@ from grow_worker.worker_types import (
 )
 
 logger = logging.getLogger("grow_worker")
+
 GROW_TASK_TYPE = GrowTaskType(os.environ.get("GROW_TASK_TYPE"))
+
+
+def run_mesido(input_esdl: str) -> str:
+    """Run mesido using the specific workflow.
+
+    Note: This is run without a subprocess! Casadi does not yield the GIL and therefore
+    causes starved thread issues.
+
+    :param input_esdl: The input ESDL XML string.
+    :return: GROW optimized or simulated ESDL
+    """
+    mesido_func = get_problem_function(GROW_TASK_TYPE)
+    mesido_workflow = get_problem_type(GROW_TASK_TYPE)
+
+    base_folder = Path(__file__).resolve().parent.parent
+    write_result_db_profiles = "INFLUXDB_HOSTNAME" in os.environ
+    influxdb_host = os.environ.get("INFLUXDB_HOSTNAME", "localhost")
+    influxdb_port = int(os.environ.get("INFLUXDB_PORT", "8086"))
+
+    logger.info("Will write result profiles to influx: {}. At {}:{}",
+                write_result_db_profiles,
+                influxdb_host,
+                influxdb_port)
+
+    solution: GROWProblem = mesido_func(
+        mesido_workflow,
+        base_folder=base_folder,
+        esdl_string=base64.encodebytes(input_esdl.encode("utf-8")),
+        esdl_parser=ESDLStringParser,
+        write_result_db_profiles=write_result_db_profiles,
+        influxdb_host=influxdb_host,
+        influxdb_port=influxdb_port,
+        influxdb_username=os.environ.get("INFLUXDB_USERNAME"),
+        influxdb_password=os.environ.get("INFLUXDB_PASSWORD"),
+        influxdb_ssl=False,
+        influxdb_verify_ssl=False,
+        update_progress_function=None,
+        profile_reader=InfluxDBProfileReader,
+    )
+
+    return cast(str, solution.optimized_esdl_string)
 
 
 def grow_worker_task(
@@ -36,33 +79,18 @@ def grow_worker_task(
     :param update_progress_handler: Handler to notify of any progress changes.
     :return: GROW optimized or simulated ESDL
     """
-    base_folder = Path(__file__).resolve().parent.parent
-    write_result_db_profiles = "INFLUXDB_HOSTNAME" in os.environ
-    influxdb_host = os.environ.get("INFLUXDB_HOSTNAME", "localhost")
-    influxdb_port = int(os.environ.get("INFLUXDB_PORT", "8086"))
+    # TODO Very nasty hack. Celery unfortunately starts the worker subprocesses as 'daemons'
+    #  which prevents this process from creating any other subprocesses. Therefore, we
+    #  acknowledge this process is a daemon and turn of the protectioon that prevents new
+    #  subprocesses from being created. This does introduce the issue that if this
+    #  process is killed/cancelled/revoked, the subprocess will continue as a zombie process.
+    #  See https://github.com/Project-OMOTES/optimizer-worker/issues/54
+    current_process()._config['daemon'] = False  # type: ignore[attr-defined]
 
-    logger.info(
-        f"Will write result profiles to influx: {write_result_db_profiles}. "
-        f"At {influxdb_host}:{influxdb_port}"
-    )
+    with multiprocessing.Pool(1) as pool:
+        output_esdl = pool.map(run_mesido, [input_esdl])[0]
 
-    solution: GROWProblem = get_problem_function(GROW_TASK_TYPE)(
-        get_problem_type(GROW_TASK_TYPE),
-        base_folder=base_folder,
-        esdl_string=base64.encodebytes(input_esdl.encode("utf-8")),
-        esdl_parser=ESDLStringParser,
-        write_result_db_profiles=write_result_db_profiles,
-        influxdb_host=influxdb_host,
-        influxdb_port=influxdb_port,
-        influxdb_username=os.environ.get("INFLUXDB_USERNAME"),
-        influxdb_password=os.environ.get("INFLUXDB_PASSWORD"),
-        influxdb_ssl=False,
-        influxdb_verify_ssl=False,
-        update_progress_function=update_progress_handler,
-        profile_reader=InfluxDBProfileReader,
-    )
-
-    return cast(str, solution.optimized_esdl_string)
+    return output_esdl
 
 
 if __name__ == "__main__":
