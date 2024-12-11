06f1ebc6dcaf636e09f5ef83208e03a0a3bdf945
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-12-11 12:24:51 +0100

    89: Update to python 3.10 and update to latest python template.

--- ci/linux/typecheck.sh
+++ ci/linux/typecheck.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then
   echo "Activating .venv first."
   . .venv/bin/activate
 fi
+
 python -m mypy ./src/grow_worker ./unit_test/
