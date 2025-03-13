d163a2aeae0859273d446e2f84b0dfe8c87c1255
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2025-03-13 22:33:56 +0100

    move mesido back to celery worker process and enable progress update funtion again

--- src/grow_worker/worker.py
+++ src/grow_worker/worker.py
@@ -1,10 +1,5 @@
 import base64
 import logging
-import multiprocessing
-import multiprocessing.pool
-import signal
-import time
-from multiprocessing.process import current_process
 import os
 from pathlib import Path
 from typing import cast, Dict, List, Tuple, Optional
@@ -34,20 +29,26 @@ GROW_TASK_TYPE = GrowTaskType(os.environ.get("GROW_TASK_TYPE"))
 class EarlySystemExit(Exception):
     """Wrapper for `SystemExit` exception.
 
-    To ensure that the worker process does not shutdown but rather handles the `SystemExit` as an
+    To ensure that the worker process does not shut down but rather handles the `SystemExit` as an
     error
     """
 
     ...
 
 
-def run_mesido(input_esdl: str) -> Tuple[Optional[str], List[EsdlMessage]]:
-    """Run mesido using the specific workflow.
+def grow_worker_task(
+    input_esdl: str, workflow_config: ProtobufDict, update_progress_handler: UpdateProgressHandler
+) -> Tuple[Optional[str], List[EsdlMessage]]:
+    """Run the grow worker task and run configured specific problem type for this worker instance.
 
-    Note: This is run without a subprocess! Casadi does not yield the GIL and therefore
-    causes starved thread issues.
+    Note: Be careful! This spawns within a subprocess and gains a copy of memory from parent
+    process. You cannot open sockets and other resources in the main process and expect
+    it to be copied to subprocess. Any resources e.g. connections/sockets need to be opened
+    in this task by the subprocess.
 
     :param input_esdl: The input ESDL XML string.
+    :param workflow_config: Extra parameters to configure this run.
+    :param update_progress_handler: Handler to notify of any progress changes.
     :return: GROW optimized or simulated ESDL and a list of ESDL feedback messages.
     """
     mesido_func = get_problem_function(GROW_TASK_TYPE)
@@ -77,7 +78,7 @@ def run_mesido(input_esdl: str) -> Tuple[Optional[str], List[EsdlMessage]]:
             influxdb_password=os.environ.get("INFLUXDB_PASSWORD"),
             influxdb_ssl=False,
             influxdb_verify_ssl=False,
-            update_progress_function=None,
+            update_progress_function=update_progress_handler,
             profile_reader=InfluxDBProfileReader,
         )
         esdl_str = cast(str, solution.optimized_esdl_string)
@@ -119,71 +120,6 @@ def parse_mesido_esdl_messages(
             )
         )
     return esdl_messages
-
-
-def kill_pool(pool: multiprocessing.pool.Pool) -> None:
-    """Terminate all the process of a multiprocessing.Pool with SIGKILL.
-
-    Found here: https://stackoverflow.com/a/47580796
-
-    multiprocessing.Pool.terminate does not provide a way to give a different signal than SIGTERM
-    so this function hooks into the internals to properly handle sending SIGKILL to all processes in
-    the pool.
-
-    :param pool: The multiprocessing to kill all processes in.
-    """
-    #
-    # stop repopulating new child
-    pool._state = multiprocessing.pool.TERMINATE  # type: ignore[attr-defined]
-    pool._worker_handler._state = multiprocessing.pool.TERMINATE  # type: ignore[attr-defined]
-    for p in pool._pool:  # type: ignore[attr-defined]
-        if p.is_alive():
-            logger.warning("Sending SIGKILL to pool process with pid %s", p.pid)
-            os.kill(p.pid, signal.SIGKILL)
-    # .is_alive() will reap dead process
-    wait_till = time.time() + 5.0
-    while (
-        any(p.is_alive() for p in pool._pool)  # type: ignore[attr-defined]
-        and time.time() < wait_till
-    ):
-        pass
-    logger.warning("All processes in pool have been terminated.")
-    pool.terminate()
-    logger.warning("Forceful pool termination completed.")
-
-
-def grow_worker_task(
-    input_esdl: str, workflow_config: ProtobufDict, update_progress_handler: UpdateProgressHandler
-) -> Tuple[Optional[str], List[EsdlMessage]]:
-    """Run the grow worker task and run configured specific problem type for this worker instance.
-
-    Note: Be careful! This spawns within a subprocess and gains a copy of memory from parent
-    process. You cannot open sockets and other resources in the main process and expect
-    it to be copied to subprocess. Any resources e.g. connections/sockets need to be opened
-    in this task by the subprocess.
-
-    :param input_esdl: The input ESDL XML string.
-    :param workflow_config: Extra parameters to configure this run.
-    :param update_progress_handler: Handler to notify of any progress changes.
-    :return: GROW optimized or simulated ESDL and a list of ESDL feedback messages.
-    """
-    # TODO Very nasty hack. Celery unfortunately starts the worker subprocesses as 'daemons'
-    #  which prevents this process from creating any other subprocesses. Therefore, we
-    #  acknowledge this process is a daemon and turn of the protectioon that prevents new
-    #  subprocesses from being created. This does introduce the issue that if this
-    #  process is killed/cancelled/revoked, the subprocess will continue as a zombie process.
-    #  See https://github.com/Project-OMOTES/optimizer-worker/issues/54
-    current_process()._config["daemon"] = False  # type: ignore[attr-defined]
-
-    with multiprocessing.Pool(1) as pool:
-        try:
-            output = pool.map(run_mesido, [input_esdl])[0]
-        except SystemExit as e:
-            logger.warning("During pool the worker was requested to quit: %s %s", type(e), e)
-            kill_pool(pool)
-            raise
-
-    return output
 
 
 if __name__ == "__main__":
